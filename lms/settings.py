f778de675f197250a1ff5bc03b6f38ebb27da85e
chaymaa101 <chamaaela02@gmail.com>
2024-02-25 15:00:51 +0100

    msg2

--- lms/settings.py
+++ lms/settings.py
@@ -125,5 +125,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #media file
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
