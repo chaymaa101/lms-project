9bc9f764b109e3afa5a01198c4ba8edfd2ef4ae7
chaymaa101 <chamaaela02@gmail.com>
2024-02-25 23:39:11 +0100

    msg3

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,21 +1,51 @@
-from django.shortcuts import render
-from .models import *# Create your views here.
-from .forms import *
+from django.shortcuts import render ,redirect, get_object_or_404
+from .models import *  # It's better to import specific models rather than using *
+from .forms import BookForm, CategoryForm  # Assuming you've defined these forms
+
 def index(request):
     if request.method == 'POST':
         add_book = BookForm(request.POST, request.FILES)
         if add_book.is_valid():
             add_book.save()
 
-    context ={
+       
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_category.save()
+
+    context = {
         'category': Category.objects.all(),
-        'books':Book.objects.all(),
-        'form':BookForm(),
+        'books': Book.objects.all(),
+        'form': BookForm(),  
+        'formcat': CategoryForm(),  
     }
     return render(request, 'pages/index.html', context)
+
 def books(request):
-    context ={
-         'category': Category.objects.all(),
-         'books':Book.objects.all(),
+    context = {
+        'category': Category.objects.all(),
+        'books': Book.objects.all(),
     }
-    return render(request, 'pages/books.html',context)
+    return render(request, 'pages/books.html', context)
+def update(request, id):
+    book_id =  Book.objects.get(id=id)
+    if request.method == 'POST':
+        book_save =BookForm(request.POST, request.FILES ,instance=book_id)
+        if book_save.is_valid():
+            book_save.save()
+            return redirect('/')
+    else:
+        book_save=BookForm(instance=book_id)
+    context={
+        'form':book_save,
+
+    }
+    return render(request, 'pages/update.html', context)
+
+
+def delete(request, id):
+    book_delete= get_object_or_404(Book, id=id)
+    if request.method == 'POST':
+        book_delete.delete()
+        return redirect('/')
+    return render(request, 'pages/delete.html')
